5713f331662523d1e0abf1a6e8e081bb2197a208
zana-abdollahpour <zana.ap4@gmail.com>
2024-07-07 18:43:31 +0330

    handle changing currentGuess

--- src/components/keyboard/VirtualKeyboard.tsx
+++ src/components/keyboard/VirtualKeyboard.tsx
@@ -1,10 +1,28 @@
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 
+import { useGameState } from "../../hooks/useGameState";
+
 export default function VirtualKeyboard() {
+  const { targetWord, currentGuess, setCurrentGuess } = useGameState();
+
   const handleKeyPress = (pressedKey: string) => {
-    // TODO: handle key press and set changes in context, remove console.log()
-    console.log(pressedKey);
+    if (pressedKey === "{backspace}") {
+      setCurrentGuess((prev) => prev.slice(0, -1));
+      return;
+    }
+    if (pressedKey === "{ent}") {
+      if (targetWord.length > currentGuess.length) return;
+      // TODO: Check to see if targetWord and current guess match
+
+      console.log(pressedKey);
+    }
+
+    const isValidLetter =
+      /^[a-zA-Z]+$/.test(pressedKey) && pressedKey.length === 1;
+    if (!isValidLetter) return;
+
+    setCurrentGuess((prev) => (prev += pressedKey.toLowerCase()));
   };
 
   return (
--- src/components/keyboard/WindowKeyPressHandler.tsx
+++ src/components/keyboard/WindowKeyPressHandler.tsx
@@ -1,23 +1,31 @@
 import { useEffect } from "react";
 
+import { useGameState } from "../../hooks/useGameState";
+
 export default function WindowKeyPressHandler() {
+  const { targetWord, currentGuess, setCurrentGuess } = useGameState();
+
   useEffect(() => {
     const keyPressHandler = (e: KeyboardEvent) => {
-      // TODO: handle key press and set changes in context, remove console.log()
       if (e.key === "Backspace") {
-        console.log("BackSpace -> pressed");
+        setCurrentGuess((prev) => prev.slice(0, -1));
       }
       if (e.key === "Enter") {
-        console.log("Enter -> pressed");
+        if (targetWord.length > currentGuess.length) return;
+        // TODO: Check to see if targetWord and current guess match
       }
+
+      if (currentGuess.length >= targetWord.length) return;
 
       const isValidLetter = /^[a-zA-Z]+$/.test(e.key) && e.key.length === 1;
       if (!isValidLetter) return;
+
+      setCurrentGuess((prev) => (prev += e.key.toLowerCase()));
     };
 
     window.addEventListener("keydown", keyPressHandler);
     return () => window.removeEventListener("keydown", keyPressHandler);
-  }, []);
+  }, [currentGuess.length, setCurrentGuess, targetWord.length]);
 
   return null;
 }
